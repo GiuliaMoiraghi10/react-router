4e57f2e6dd7a2df500ce77f600520b6ee3e54e7f
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-06 15:49:13 +0100

    Aggiunta Navbar e collegamento DefaultLayout

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './Pages/HomePage.jsx'
 import About from './Pages/About.jsx'
 import Contact from './Pages/Contact.jsx'
+import DefaultLayout from './Layout/DefaultLayout.jsx'
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
+        <Route element={<DefaultLayout />}>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,20 @@
+import { NavLink } from 'react-router-dom'
+import style from './Navbar.module.css'
+
+export default function Navbar() {
+    return (
+        <nav className={style.nav}>
+            <ul>
+                <li>
+                    <NavLink to='/'>HomePage</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/about'>About</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/contact'>Contact</NavLink>
+                </li>
+            </ul>
+        </nav>
+    )
+}
