52c980ad455b663747a6ac5c775adf7d60ca6d75
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-09 11:45:18 +0100

    Modifiche alla HomePage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,8 +1,21 @@
+import imageHome from '../assets/harryPotterHome.jpg'
+import style from './HomePage.module.css'
+import Card from '../Components/Card'
+
 export default function HomePage() {
+
     return (
         <div>
-            <h1>La magia di Harry Potter</h1>
+            <h1 className={style.title}>La magia di Harry Potter</h1>
+            <img className={style.imgHome} src={imageHome} alt="" />
             <h2>Scegli il personaggio</h2>
+            <section className={style.container}>
+                <div className={style.row}>
+                    <div className={style.col_4}>
+                        <Card />
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
