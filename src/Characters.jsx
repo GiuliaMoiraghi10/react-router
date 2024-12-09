3992556006ea2983bb56e8fdf0a197bf3041a7ce
GiuliaMoiraghi10 <gmoiraghi10@gmail.com>
2024-12-09 11:44:24 +0100

    Aggiungo array di oggetti - characters

---
+++ src/Characters.jsx
@@ -0,0 +1,39 @@
+import imgHarry from './assets/harryPotter.webp'
+import imgHermione from './assets/hermione.jpg'
+import imgRon from './assets/ron.webp'
+import imgVoldemort from './assets/voldemort.jpg'
+import imgSilente from './assets/silente.webp'
+import imgHagrid from './assets/hagrid.webp'
+
+export const characters = [
+    {
+        id: 1,
+        name: 'Harry Potter',
+        image: '',
+    },
+    {
+        id: 2,
+        name: 'Hermione Granger',
+        image: '',
+    },
+    {
+        id: 3,
+        name: 'Ron Weasley',
+        image: '',
+    },
+    {
+        id: 4,
+        name: 'Voldemort',
+        image: '',
+    },
+    {
+        id: 5,
+        name: 'Albus Silente',
+        image: '',
+    },
+    {
+        id: 6,
+        name: 'Hagrid',
+        image: '',
+    },
+]
